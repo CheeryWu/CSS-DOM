65e308ed9406d9652465e332fd37736fede715b5
Cheery_Wu <601505839@qq.com>
2017-03-16 08:26:24 +0800

    使message元素每次以1像素的单位位移

--- scripts/positionMessage.js
+++ scripts/positionMessage.js
@@ -8,12 +8,32 @@ function positionMessage() {
   elem.style.position = "absolute";
   elem.style.top = "50px";
   elem.style.left = "100px";
-  movement = setTimeout("moveMessage()", 5000);
 }
 function moveMessage() {
   if (!document.getElementById) return false;
   if (!document.getElementById("message")) return false;
-  var elem = document.getElementById("message")
-  elem.style.left = "200px";
+  var elem = document.getElementById("message");
+
+  var xpos = parseInt(elem.style.left);
+  var ypos = parseInt(elem.style.top);
+  if (xpos == 200  &&  ypos == 100) {
+    return true;
+  }
+  if (xpos < 200) {
+    xpos ++;
+  }
+  if (xpos > 200) {
+    xpos --;
+  }
+  if (ypos < 100) {
+    ypos ++;
+  }
+  if (ypos > 100) {
+    ypos --;
+  }
+  elem.style.left = xpos + "px";
+  elem.style.top = ypos + "px";
+  moveMessage("movaMessages()",10);
 }
 addLoadEvent(positionMessage);
+addLoadEvent(moveMessage);
