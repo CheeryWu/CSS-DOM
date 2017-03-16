04dd58b3600cd844ba715b9de2c2a899eb37d26f
Cheery_Wu <601505839@qq.com>
2017-03-16 08:09:02 +0800

    用JavaScript实现动画效果

---
+++ scripts/positionMessage.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 吴晨 on 2017/3/16.
+ */
+function positionMessage() {
+  if (!document.getElementById) return false;
+  if (!document.getElementById("message")) return false;
+  var elem = document.getElementById("message");
+  elem.style.position = "absolute";
+  elem.style.top = "50px";
+  elem.style.left = "100px";
+}
+addLoadEvent(positionMessage);
